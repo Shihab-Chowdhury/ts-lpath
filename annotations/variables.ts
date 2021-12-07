cb16ba351e9b5e4276628536e2f548b747e1f203
Shihab <cshihab04@gmail.com>
2021-12-07 17:43:38 +0600

    A

--- annotations/variables.ts
+++ annotations/variables.ts
@@ -23,11 +23,16 @@ let point: { x: number; y: number } = {
     y: 20
 };
 
-// function      |<--  annotation  -->| 
+// function     |<--  annotation  -->| 
 const logNumber: (i: number) => void = (i: number) => {
     console.log(i);
     
 };
 
+// When to use annotations
+// 1) Function that returns the 'any' type
+const json = '{"x": 10, "y": 20}';
+const coordinates = JSON.parse(json);
+console.log(coordinates); // {x: 10, y: 20}
 
 
--- ts-note.md
+++ ts-note.md
@@ -1,1 +1,2 @@
 017 Understanding Inferences : 
+020
